c571ab4d837a27549948f16bd08cc1cc16a16bfe
jshukle <john@zevross.com>
2026-02-06 19:27:27 -0500

    url parameter JS in UI and observers in server

--- R/server.R
+++ R/server.R
@@ -12,21 +12,53 @@ server <- function(input, output, session) {
   # useful for debugging; can comment off if not using
   session_id <- session$token
 
-  
-# Show sever message & reload button
+  # Show sever message & reload button
   sever::sever()
 
-  # App Server logic here
+  # Parse URL on app load
+  shiny::observe({
+    query <- shiny::parseQueryString(session$clientData$url_search)
+    
+    if (!is.null(query$tab)) {
+      bslib::nav_select(id = "navbar_page", selected = query$tab, session = session)
+    }
+  })
+  
+  # Update URL when main tab changes
+  # shiny::observeEvent(input$navbar_page, {
+  #   query <- shiny::parseQueryString(session$clientData$url_search)
+  #   query$tab <- input$navbar_page
+    
+  #   # Clear subtab when main tab changes
+  #   query$subtab <- NULL
+    
+  #   query_string <- paste(names(query), query, sep = "=", collapse = "&")
+  #   shiny::updateQueryString(paste0("?", query_string), mode = "push")
+  # }, ignoreInit = TRUE)
+
+  # Handle URL parameter restoration
+  shiny::observeEvent(input$url_params_restore, {
+    params <- input$url_params_restore
+    
+    # Restore main tab first
+    if (!is.null(params$navbar_page)) {
+      tab_name <- gsub('^"|"$', '', params$navbar_page)
+      bslib::nav_select(id = "navbar_page", selected = tab_name, session = session)
+    }
+    
+    # Send restoration signal to modules with all params
+    session$userData$restore_params <- params
+  }, ignoreInit = TRUE)
 
   # Call Modules
-  observeEvent(c(req(input$navbar_page == "Country Explorer")), once = TRUE, {
-    country_module_server("country")
+  shiny::observeEvent(c(req(input$navbar_page == "Country Explorer")), once = TRUE, {
+    country_module_server("country", session)
   })
-  observeEvent(c(req(input$navbar_page == "Region Explorer")), once = TRUE, {
-    region_module_server("region")
+  shiny::observeEvent(c(req(input$navbar_page == "Region Explorer")), once = TRUE, {
+    region_module_server("region", session)
   })
-  observeEvent(c(req(input$navbar_page == "Globe")), once = TRUE, {
-    globe_module_server("globe")
+  shiny::observeEvent(c(req(input$navbar_page == "Globe")), once = TRUE, {
+    globe_module_server("globe", session)
   })
   
 }
--- R/ui.R
+++ R/ui.R
@@ -27,7 +27,86 @@ ui <- function(req) {
         # shiny::tags$link(rel = "icon", href = file.path(pkgload::pkg_name(), "img/ifc_logo.svg")),
         shiny::tags$link(href = file.path(pkgload::pkg_name(), "css/custom-styles.min.css"), rel = "stylesheet", type = "text/css"),
         shiny::tags$script(src = file.path(pkgload::pkg_name(), "js/custom-js.js"))
-      )
+      ),
+
+      tags$script(HTML("
+        var urlUpdateTimeout;
+        var isInitialLoad = true;
+        
+        // Build query string from ALL current inputs
+        function buildQueryString() {
+          if (!Shiny || !Shiny.shinyapp || !Shiny.shinyapp.$inputValues) {
+            return '';
+          }
+          
+          var inputs = Shiny.shinyapp.$inputValues;
+          var params = [];
+          
+          for (var key in inputs) {
+            if (inputs.hasOwnProperty(key)) {
+              var value = inputs[key];
+              if (value !== null && value !== undefined) {
+                if (Array.isArray(value)) {
+                  params.push(key + '=' + encodeURIComponent(JSON.stringify(value)));
+                } else if (typeof value === 'string') {
+                  params.push(key + '=' + encodeURIComponent('\"' + value + '\"'));
+                } else if (typeof value === 'boolean' || typeof value === 'number') {
+                  params.push(key + '=' + encodeURIComponent(value));
+                }
+              }
+            }
+          }
+          
+          return params.length > 0 ? '?' + params.join('&') : '';
+        }
+        
+        // Update browser URL
+        function updateBrowserUrl() {
+          // NEVER update on initial load - only after user interactions
+          if (isInitialLoad) {
+            return;
+          }
+          
+          var queryString = buildQueryString();
+          if (queryString) {
+            var newUrl = window.location.pathname + queryString;
+            history.replaceState(null, '', newUrl);
+          }
+        }
+        
+        // Restore inputs from URL on page load
+        function restoreFromUrl() {
+          var urlParams = new URLSearchParams(window.location.search);
+          var paramsObj = {};
+          
+          urlParams.forEach(function(value, key) {
+            paramsObj[key] = value;
+          });
+          
+          // Send to Shiny for processing
+          if (Object.keys(paramsObj).length > 0) {
+            Shiny.setInputValue('url_params_restore', paramsObj, {priority: 'event'});
+          }
+          
+          // After restoration signal is sent, allow URL updates from user interactions
+          setTimeout(function() {
+            isInitialLoad = false;
+          }, 3000);
+        }
+        
+        // Listen for input changes (but skip during initial load)
+        $(document).on('shiny:inputchanged', function(event) {
+          if (!isInitialLoad) {
+            clearTimeout(urlUpdateTimeout);
+            urlUpdateTimeout = setTimeout(updateBrowserUrl, 500);
+          }
+        });
+        
+        // Restore on initial connection
+        $(document).on('shiny:connected', function() {
+          setTimeout(restoreFromUrl, 500);
+        });
+      "))
     ),
     # footer = shiny::tags$footer(
     #   id = "zrsa-footer",
