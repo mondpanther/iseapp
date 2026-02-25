fcfd46190c8cf72f21e0f4174f1954bf0a939527
jshukle <john@zevross.com>
2026-02-25 10:11:25 -0500

    app start edits for development

--- app.R
+++ app.R
@@ -20,8 +20,8 @@ pkgload::load_all(export_all = FALSE,
                   attach_testthat = FALSE)
 # Override Positron's Shiny browser setting
 options(shiny.launch.browser = function(url) {
-  utils::browseURL(url) # for deployments
-  # utils::browseURL(url, browser = "C:/Program Files/Google/Chrome/Application/chrome.exe") # for development
+  # utils::browseURL(url) # for deployments
+  utils::browseURL(url, browser = "C:/Program Files/Google/Chrome/Application/chrome.exe") # for development
 })
 innovationStrategyExplorer::runAppPackage()
 
